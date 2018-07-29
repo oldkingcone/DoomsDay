f71eb302e53d7ee38bc30620d1fab04c76f3099f
OldKingCone <11233163+oldkingcone@users.noreply.github.com>
2018-07-28 18:46:34 -0700

    Update DoomsDayMain.py
    Database is evenly created, and stores "Names" and "Hashed Passwords" in relation to each other. Working on a way to generate an email address from the generated names, giving it uniqueness.

--- DoomsDayMain.py
+++ DoomsDayMain.py
@@ -1,7 +1,9 @@
-#decided to go with twisted, much more easy to work with.
+# decided to go with twisted, much more easy to work with.
 
 try:
     import sqlite3
+    import names
+    import hashlib
     from twisted.web.server import Site
     from twisted.web.static import File
     from twisted.internet import reactor
@@ -10,22 +12,45 @@ try:
     from time import sleep
     from random import randint
 except ImportError as e:
-    print("Sorry... Something went wrong, try running pip install -r REQUIREMENTS and run the app again. \n {}".format(e))
+    print(
+        "Sorry... Something went wrong, try running pip install -r REQUIREMENTS and run the app again. \n {}".format(e))
     import sys
     sys.exit(1)
+
 os.system("mkdir ./wwwroot")
 os.system("cd ./wwwroot")
-    
+
 database = sqlite3.connect('admin_databse.sqlite')
 c = database.cursor()
+#creating the junk filled and falsely encrypted database(make it mimic a glob)
 c.execute('''CREATE TABLE IF NOT EXISTS Site_Info(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, jointime TIMESTAMP DEFAULT 
             CURRENT_TIMESTAMP NOT NULL, username TEXT, email TEXT, password TEXT)''')
-sql_stmt = "INSERT INTO Site_Info(username, email, password) VALUES ('%s','%s','%s')"
+sql_stmt = "INSERT INTO Site_Info(username, email, password) VALUES ('%s', '%s', '%s')"
 sql_stmt = str(sql_stmt)
+
+def name_generate():
+    digest_name = "INSERT INTO Site_Info(username, password) VALUES ('%s', '%s')"
+    namer = set()
+    for i in range(100):
+        name = names.get_full_name()
+        namer.add(name)
+        print(name)
+        database.commit()
+        style = name.encode()
+        hashed_pass = hashlib.sha512(style).hexdigest()
+        c.execute(digest_name % (name, hashed_pass))
+        database.commit()
+
+name_generate()
+c.close()
+
+
 def random_seeding(time):
-    #going to be very hackish here.. sorry.
+    # going to be very hackish here.. sorry.
     sleep(time)
-    
+
+#@todo set up on connection recieved, so we are not hogging precious system resources.
+
 resource = File('./wwwroot')
 factory = Site(resource)
 endpoint = endpoints.TCP4Server(reactor, 8888)
