a05cdfc4aff1cde863d6777d298cd980f292861b
OldKingCone <11233163+oldkingcone@users.noreply.github.com>
2018-07-29 12:35:45 -0700

    Update DoomsDayMain.py

--- DoomsDayMain.py
+++ DoomsDayMain.py
@@ -23,29 +23,36 @@ os.system("cd ./wwwroot")
 database = sqlite3.connect('admin_databse.sqlite')
 c = database.cursor()
 #creating the junk filled and falsely encrypted database(make it mimic a glob)
+#@todo database is complete, just missing the email address generation.
+#@todo make a relational database, add some more hashes maybe sha1 and faked access times.
 c.execute('''CREATE TABLE IF NOT EXISTS Site_Info(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, jointime TIMESTAMP DEFAULT 
             CURRENT_TIMESTAMP NOT NULL, username TEXT, email TEXT, password TEXT)''')
+#@todo, ok so this works. Need to find a way to populate legit looking IP address's
+#@todo
+''' or just take all the IP address's from lastb and cram them in here, 
+    that way it all resolves back to "legit" sources.'''
+c.execute('''CREATE TABLE IF NOT EXISTS Access_times(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT,
+            FORIGN_KEY email REFERENCES username, Accessed_Last CURRENT_TIMESTAMP NOT NULL, Login_IP TEXT)''')
 sql_stmt = "INSERT INTO Site_Info(username, email, password) VALUES ('%s', '%s', '%s')"
 sql_stmt = str(sql_stmt)
 
 def name_generate():
     digest_name = "INSERT INTO Site_Info(username, password) VALUES ('%s', '%s')"
-    #@todo, need to make this generate "valid" looking email addresses, so this database looks as legit as possible.
+    namer = set()
     for i in range(100):
         name = names.get_full_name()
+        namer.add(name)
+        print(name)
         database.commit()
         style = name.encode()
         hashed_pass = hashlib.sha512(style).hexdigest()
         c.execute(digest_name % (name, hashed_pass))
         database.commit()
 
+
 name_generate()
 c.close()
 
-
-def random_seeding(time):
-    # going to be very hackish here.. sorry.
-    sleep(time)
 
 #@todo set up on connection recieved, so we are not hogging precious system resources.
 
