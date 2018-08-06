3fe8d082e2651f7643f925c8d43c778eb2d3bbaa
OldKingCone <11233163+oldkingcone@users.noreply.github.com>
2018-08-05 18:27:35 -0700

    Update email_chooser.py

--- email_chooser.py
+++ email_chooser.py
@@ -1,17 +1,48 @@
+import random
 
+#-----------------------------------------------------------------------------------------------------------------------------
+# will make this more interactive, may use a class here.    
+domains_list = ["gmail.com",
+                "yahoo.com",
+                "yandex.com",
+                "protonmail.com",
+                "hotmail.com",
+                "1and1.com",
+                "mail.com",
+                "inbox.com",
+                "aol.com",
+                "outlook.com",
+                "icloud.com",
+                "office365.com",
+                "zoho.mail",
+                "hushmail.com",
+                "fastmail.com",
+                "gmxmail.com"]
+words = ["barbie",
+         "rocker",
+         "diamond",
+         "leet",
+         "beer",
+         "rockstar",
+         "bulleit",
+         "moon",
+         "draught",
+         "tanaka",
+         "yankee",
+         "aviator",
+         "queen",
+         "cocktail",
+         "absolute",
+         "mountain",
+         "light",
+         "yankee",
+         "beef"
+         ]
+#---------------------------------------------------------
 
-
-
-def getParagraph(selection):
-    switcher = {
-        1: para1,
-        2: para2,
-        3: para3,
-        4: para4,
-        5: para5,
-        6: para6,
-        7: para7,
-        8: para8,
-        9: para9,
-    }
-    return switcher.get(selection)
+def genEmail(iteration):
+    email_list = set()
+    for i in range(iteration):
+        email = random.choice(words) + random.choice(words) + '@' + random.choice(domains_list)
+        email_list.add(email)
+    return email_list
